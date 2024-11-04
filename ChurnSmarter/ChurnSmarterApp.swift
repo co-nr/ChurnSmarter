1375460de4611633a6e9e2155132532a153238b6
Connor Gill <connor@cgill.net>
2024-11-04 19:59:42 +0700

    Initial Commit

---
+++ ChurnSmarter/ChurnSmarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChurnSmarterApp.swift
+//  ChurnSmarter
+//
+//  Created by Connor on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ChurnSmarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChurnSmarterTests/ChurnSmarterTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChurnSmarterTests.swift
+//  ChurnSmarterTests
+//
+//  Created by Connor on 11/4/24.
+//
+
+import Testing
+@testable import ChurnSmarter
+
+struct ChurnSmarterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
